fee2ad9dd6d0c37c20686dfd32deae856fc636d6
laiqixiao <laiqixiao214@gmai.com>
2024-02-28 18:57:19 -0500

    A number is a multiple of ‘5’ return “Buzz” test pass

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,6 +1,7 @@
 public class FizzBuzz {
     public static String calculate(int number) {
         if (number % 3 == 0) return "Fizz";
+        if (number % 5 == 0) return "Buzz";
         return "1";
         //test
     }
