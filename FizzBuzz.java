78c162362ed1c0f7a64e70b2bc5829e9dcb22fbf
laiqixiao <laiqixiao214@gmai.com>
2024-02-28 19:18:28 -0500

    Add comments and refactor to make code easy to understand

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,8 +3,6 @@ public class FizzBuzz {
         if (number % 15 == 0) return "FizzBuzz";
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
-
         return String.valueOf(number);
-        //test
     }
 }
--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -4,33 +4,36 @@ import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
     @Test
-    public void testNumberOneReturnsStringOne() {
-        assertEquals("1", FizzBuzz.calculate(1));
-    }
-    @Test
     public void whenMultipleOfThreeThenReturnFizz() {
         assertEquals("Fizz", FizzBuzz.calculate(3));
         assertEquals("Fizz", FizzBuzz.calculate(6));
         assertEquals("Fizz", FizzBuzz.calculate(9));
+        //A number is a multiple of ‘3’ return “Fizz”
+        // Add more cases if necessary
     }
     @Test
     public void whenMultipleOfFiveThenReturnBuzz() {
         assertEquals("Buzz", FizzBuzz.calculate(5));
         assertEquals("Buzz", FizzBuzz.calculate(10));
         assertEquals("Buzz", FizzBuzz.calculate(20));
+        //A number is a multiple of ‘5’ return “Buzz”
+        // Add more cases if necessary
     }
     @Test
     public void whenMultipleOfBothThreeAndFiveThenReturnFizzBuzz() {
         assertEquals("FizzBuzz", FizzBuzz.calculate(15));
         assertEquals("FizzBuzz", FizzBuzz.calculate(30));
         assertEquals("FizzBuzz", FizzBuzz.calculate(45));
+        //A number is a multiple of both ‘3’ and ‘5’ return “FizzBuzz”
+        // Add more cases if necessary
     }
     @Test
     public void whenNotMultipleOfThreeOrFiveThenReturnNumber() {
         assertEquals("1", FizzBuzz.calculate(1));
         assertEquals("2", FizzBuzz.calculate(2));
-        assertEquals("4", FizzBuzz.calculate(4));
-        // Add more cases to thoroughly test this scenario
+        // A number is not divisible by ‘3’ or ‘5’ return the number itself (as a String)
+        // Add more cases if necessary
+
     }
 
 
